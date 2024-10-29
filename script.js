604ba2e308a97b7e4a6d266e36eb5c2d74935652
abdul Razzaq <ar8937562@gmail.com>
2024-10-29 23:21:18 +0500

    console and window functions in js

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log('Hello');
+console.log('We are Pakistani');
+
+window.alert('This is an alert message');
+window.alert('alert 2');
+
+window.prompt('whats ur name ?');
+
+window.confirm('Are you sure you want to delete this file?');
