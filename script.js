6f8c36c7909f745d3cc6bb6387d32fe2ec87cb09
abdul Razzaq <ar8937562@gmail.com>
2024-10-29 23:29:57 +0500

    getting element by ID and getting content through js

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-console.log('Hello');
-console.log('We are Pakistani');
+// console.log('Hello');
+// console.log('We are Pakistani');
 
-window.alert('This is an alert message');
-window.alert('alert 2');
+// window.alert('This is an alert message');
+// window.alert('alert 2');
 
-window.prompt('whats ur name ?');
+// window.prompt('whats ur name ?');
 
-window.confirm('Are you sure you want to delete this file?');
+// window.confirm('Are you sure you want to delete this file?');
+
+
+
+document.getElementById("h1").textContent='this is H1';
+document.getElementById("p1").textContent='this is p1 element';
+
+document.getElementById("h1").textContent = 'My website';
+document.getElementById("p1").textContent = "hdash sahd ashd hae hqwjhw ahhwah ajhwhqh ahwhq ahwh ahwha ahwh ewhh qjhwh jhwahq anwhq awhhq ehjhw ahhsw wjhehw ahwhq wqj qjq "
