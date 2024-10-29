ecf1781d3ab545c48571b6833247392eae4a4d09
abdul Razzaq <ar8937562@gmail.com>
2024-10-30 00:02:02 +0500

    number (int) variables in js

--- script.js
+++ script.js
@@ -10,8 +10,27 @@
 
 
 
-document.getElementById("h1").textContent='this is H1';
-document.getElementById("p1").textContent='this is p1 element';
+// document.getElementById("h1").textContent='this is H1';
+// document.getElementById("p1").textContent='this is p1 element';
 
-document.getElementById("h1").textContent = 'My website';
-document.getElementById("p1").textContent = "hdash sahd ashd hae hqwjhw ahhwah ajhwhqh ahwhq ahwh ahwha ahwh ewhh qjhwh jhwahq anwhq awhhq ehjhw ahhsw wjhehw ahwhq wqj qjq "
+// document.getElementById("h1").textContent = 'My website';
+// document.getElementById("p1").textContent = "hdash sahd ashd hae hqwjhw ahhwah ajhwhqh ahwhq ahwh ahwha ahwh ewhh qjhwh jhwahq anwhq awhhq ehjhw ahhsw wjhehw ahwhq wqj qjq "
+
+
+
+
+
+let x
+x = 1220
+console.log(x);
+
+let age = 21;
+let height = 5.11;
+let weight = 70;
+
+console.log(typeof age);
+console.log(typeof height);
+console.log(typeof weight);
+console.log(`you are ${age} years old`);
+console.log(`your height is ${height} ft`);
+console.log(`and your weight is ${weight} kg`);
